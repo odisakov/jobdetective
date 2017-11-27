527a64f362dc93483c402b1004eef787cd2104f3
Daniel Levelev <levelev@gmail.com>
2017-11-27 21:40:35 +0100

    seeds v1

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,44 @@
 require 'faker'
 
-techs = %w(rails php asp python facebook adowrds git css html javascript)
+# => User does not seed properly, Argument Error,
+# user has no Name/Company ID
+50.times do
+  u = User.create!(
+    # Faker::Name.name,
+    # company_id: rand(1..10)
+    email: Faker::Internet.email,
+    password: Faker::Cat.registry
 
-techs.each do |technology|
-  t = Tech.create!(
-    name: technology
     )
 end
 
+tools = %w(rails php asp python facebook adwords git css html javascript xml)
+
+tools.each do |tool|
+  t = Tool.create!(
+    name: tool
+    )
+end
 
 
 10.times do
   c = Company.create!(
-    name: Faker::SiliconValley.company,
+    name: Faker::SiliconValley.company
+    )
+end
+
+50.times do
+  ct = CompanyTool.create!(
+    tool_id: rand(1..11),
+    company_id: rand(1..10)
     )
 end
 
 
-# 30.times do
-#   u = User.create!(
-#     name: Faker::Name.name,
-#     company_id: rand(1..10),
-#     )
-# end
 
-30.times do
-  ct = CompanyTech.create!(
-    company_id: rand(1..10),
-    tech_id: rand(1..10)
+100.times do
+  ct = UserTool.create!(
+    tool_id: rand(1..11),
+    user_id: rand(1..50)
     )
 end
