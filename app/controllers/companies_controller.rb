bfe0d248080f8a68e6daf09aee15cd997214a3f4
Alexander Pieczka <alexanderpieczka@ewe.net>
2017-11-29 15:27:58 +0100

    Search with multiple things in searchbar, working

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,10 +1,26 @@
 class CompaniesController < ApplicationController
   def index
-    params[:search] ||= {}
-    @companies = @companies.where(tool: params[:search][:tool]) if params[:search][:tool].present?
+    # @companies = Company.all
+
+    @companies = Company.joins(:company_tools).joins(:tools).where(:tools => { name: [params[:tool].split(",")] })
+    # @companies = @companies.where(:tools => { name: 'ruby' } ).all
+
+    # if params[:search]
+    #   @companies = Company.includes(:company_tools)
+    #   raise
+    #   @companies = @companies.where(tool: params[:search][:tool]) if params[:search][:tool]
+
+    # else
+    #   @companies = Company.all
+    # end
+    # @companies = @companies.where(tool: params[:search][:tool]) if params[:search][:tool]
   end
 
   def show
     @company = Company.find(params[:id])
   end
 end
+
+
+
+# Company.first.company_tools.first.tool
