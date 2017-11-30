9a6365b1a1b88f7a3bec093213855310c8e14b31
Daniel Levelev <levelev@gmail.com>
2017-11-30 15:56:57 +0100

    company csv works

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,13 @@
+require 'csv'
 require 'faker'
 
-tools = %w(rails php asp python facebook adowrds git css html javascript)
+
+
+
+
+
+
+tools = %w(rails php asp python facebook adwords git css html javascript)
 role = %w(ceo engineering sales)
 
 tools.each do |technology|
@@ -9,18 +16,31 @@ tools.each do |technology|
     )
 end
 
+csv_options = { col_sep: ',', quote_char: '"', headers: :first_row }
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'crunch.csv'))
 
-
-100.times do
-  c = Company.create!(
-    name: Faker::SiliconValley.company,
-    logo_url: "http://khppu.com/assets/default_user_company_logo-3058b28cca9e293f85b78add4842bc64.png",
-    city: "Berlin",
-    country: "Germany",
-    short_description: Faker::SiliconValley.motto
-    )
-  # p "Created company #{c}"
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  c = Company.new
+  c.name = row['name']
+  c.city = row['location_city']
+  c.country = row['location_country_code']
+  c.short_description = row['short_description']
+  c.logo_url = row['profile_image_url']
+  c.save
 end
+
+
+# 100.times do
+#   c = Company.create!(
+#     name: Faker::SiliconValley.company,
+#     logo_url: "http://khppu.com/assets/default_user_company_logo-3058b28cca9e293f85b78add4842bc64.png",
+#     city: "Berlin",
+#     country: "Germany",
+#     short_description: Faker::SiliconValley.motto
+#     )
+#   # p "Created company #{c}"
+# end
 
 
 100.times do
