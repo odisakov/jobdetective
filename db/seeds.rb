268c9de305a74f8f566c2e5e9756ac2773e15a7a
Daniel Levelev <levelev@gmail.com>
2017-11-30 18:40:45 +0100

    bw scraping, no name

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
 require 'csv'
 require 'faker'
-# require 'open-uri'
-# require 'nokogiri'
+require 'open-uri'
+require 'nokogiri'
+
+builtwith = "https://builtwith.com/"
 
 # file = "/Users/daniel/Downloads/built.html"
 # doc = Nokogiri::HTML(File.open(file), nil, 'utf-8')
@@ -15,6 +17,7 @@ csv_text = File.read(Rails.root.join('lib', 'seeds', 'crunch.csv'))
 
 csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
 csv.each do |row|
+  #Create Company
   c = Company.new
   c.name = row['name']
   c.city = row['location_city']
@@ -24,6 +27,45 @@ csv.each do |row|
   c.homepage_url = row['homepage_url']
   c.homepage_domain = row['homepage_domain']
   c.save
+
+  p "Created Company #{c.name}"
+
+
+  # Scraping Techs for Company
+  bw_url = "#{builtwith}#{c.homepage_domain}"
+  p "Start scraping #{bw_url}"
+  doc = Nokogiri::HTML(open(bw_url), nil, 'utf-8')
+  bw_tools = []
+  doc.search('.techItem > h3').each do |element|
+    bw_tools << element.text.strip
+  end
+  p "Scraping complete"
+
+  # for each tool check if exits
+  bw_tools.each do |tool|
+    if tool = Tool.find_by_name(tool)
+      current_tool = tool
+      p "#{current_tool} found in DB."
+      ct = CompanyTool.create!(
+        company: c,
+        tool: current_tool
+        )
+      c.save
+      p "CompanyTool created"
+    else
+      p "#{current_tool} NOT found in DB. Creating.."
+      new_tool = Tool.create!
+      new_tool.name = current_tool
+      new_tool.save
+      ct = CompanyTool.create!(
+        company: c,
+        tool: new_tool
+        )
+      c.save
+      p "CompanyTool created"
+
+    end
+  end
 end
 
 
