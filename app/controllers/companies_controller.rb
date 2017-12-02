608691a231902c84eaadabc67473758d6d753b24
Alexander Pieczka <alexanderpieczka@ewe.net>
2017-12-02 12:26:39 +0100

    Search updated

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,9 +1,11 @@
 class CompaniesController < ApplicationController
   def index
-    # @companies = Company.all
 
     if params[:tool]
-      @companies = Company.joins(:company_tools).joins(:tools).where(:tools => { name: [params[:tool].split(" ")] }).distinct
+      # @companies = Company.joins(:company_tools).joins(:tools).where(:tools => { name: [params[:tool].split(",")] }).distinct
+
+      @companies = Company.joins(:company_tools).joins(:tools).where("tools.name ILIKE ?", "%#{ params[:tool] }%" ).distinct
+
     else
       @companies = Company.all
     end
