b4d77244d7a00dee29bc8c6ee0c3739b609ae70d
Daniel Levelev <levelev@gmail.com>
2017-12-03 10:01:22 +0100

    started branch

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,105 @@ require 'csv'
 require 'faker'
 require 'open-uri'
 require 'nokogiri'
+
+
+# Create Companies from Crunchbase CSV
+csv_options = { col_sep: ',', quote_char: '"', headers: :first_row }
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'crunch.csv'))
+
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  #Create Company
+  c = Company.new
+  c.name = row['name']
+  c.city = row['location_city']
+  c.country = row['location_country_code']
+  c.short_description = row['short_description']
+  c.logo_url = row['profile_image_url']
+  c.homepage_url = row['homepage_url']
+  c.homepage_domain = row['homepage_domain']
+  c.save
+  p "Created Company #{c.name}"
+  # end
+
+
+  # Scraping Techs for Company - STACKSHARE
+  stackshare = "https://stackshare.io/"
+  company_name = c.name.downcase.gsub(" se", "").gsub(" gmbh", "").gsub(" ag", "").gsub(" ug", "").gsub(" ","-")
+  stack_url = "#{stackshare}#{company_name}/#{company_name}"
+
+  p "Fetching #{stack_url}"
+  stack_tools = []
+  begin
+    doc = Nokogiri::HTML(open(stack_url), nil, 'utf-8')
+  rescue
+    puts "404 rescued"
+    c.destroy!
+    puts "#{c.name} deleted"
+  end
+  unless doc == nil
+    doc.search('.stack-service-name-under').each do |element|
+      stack_tools << element.text.strip
+      # p "Stack_tools for #{c.name} #{stack_tools}"
+    end
+
+    # new_tools = []
+    # existing_tools = []
+
+    stack_tools.each do |tool|
+      current_tool = Tool.find_by_name("tool")
+      if current_tool
+         ct = CompanyTool.create!(
+        company: c,
+        tool: current_tool
+        )
+      else
+        new_tool = Tool.create!(
+          name: tool
+          )
+        ct = CompanyTool.create!(
+        company: c,
+        tool: new_tool
+        )
+      end
+    end
+  end
+end
+
+
+# Create Employees - Faker
+role = %w(management engineering sales)
+
+50.times do
+  pic_uid = rand(1..10)
+  u = User.create!(
+    # name: Faker::Name.name,
+    company: Company.order("RANDOM()").first,
+    email: Faker::Internet.email,
+    password: Faker::Crypto.md5,
+    linkedin_pic_url: "http://lorempixel.com/100/100/people/#{pic_uid}",
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    employment_role: role.sample
+    )
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # # require 'clearbit'
 # # Clearbit.key = ENV['CLEARBIT']
 
@@ -99,67 +198,6 @@ require 'nokogiri'
 # # # #   puts "#{index + 1}. #{element.text.strip}"
 # # # # end
 
-csv_options = { col_sep: ',', quote_char: '"', headers: :first_row }
-csv_text = File.read(Rails.root.join('lib', 'seeds', 'crunch.csv'))
-
-csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
-csv.each do |row|
-  #Create Company
-  c = Company.new
-  c.name = row['name']
-  c.city = row['location_city']
-  c.country = row['location_country_code']
-  c.short_description = row['short_description']
-  c.logo_url = row['profile_image_url']
-  c.homepage_url = row['homepage_url']
-  c.homepage_domain = row['homepage_domain']
-  c.save
-  p "Created Company #{c.name}"
-  # end
-
-
-  # Scraping Techs for Company - STACKSHARE
-  stackshare = "https://stackshare.io/"
-  company_name = c.name.downcase.gsub(" se", "").gsub(" gmbh", "").gsub(" ag", "").gsub(" ug", "").gsub(" ","-")
-  stack_url = "#{stackshare}#{company_name}/#{company_name}"
-
-  p "Fetching #{stack_url}"
-  stack_tools = []
-  begin
-    doc = Nokogiri::HTML(open(stack_url), nil, 'utf-8')
-  rescue
-    puts "404 rescued"
-    c.destroy!
-    puts "#{c.name} deleted"
-  end
-  unless doc == nil
-    doc.search('.stack-service-name-under').each do |element|
-      stack_tools << element.text.strip
-      # p "Stack_tools for #{c.name} #{stack_tools}"
-    end
-
-    # new_tools = []
-    # existing_tools = []
-
-    stack_tools.each do |tool|
-      current_tool = Tool.find_by_name("tool")
-      if current_tool
-         ct = CompanyTool.create!(
-        company: c,
-        tool: current_tool
-        )
-      else
-        new_tool = Tool.create!(
-          name: tool
-          )
-        ct = CompanyTool.create!(
-        company: c,
-        tool: new_tool
-        )
-      end
-    end
-  end
-end
 
 #   # stack_query_url = "https://stackshare.io/search/q=zalando"
 #   # doc = Nokogiri::HTML(open(stack_query_url), nil, 'utf-8')
@@ -227,22 +265,6 @@ end
 #     )
 # end
 
-
-role = %w(management engineering sales)
-
-50.times do
-  pic_uid = rand(1..10)
-  u = User.create!(
-    # name: Faker::Name.name,
-    company: Company.order("RANDOM()").first,
-    email: Faker::Internet.email,
-    password: Faker::Crypto.md5,
-    linkedin_pic_url: "http://lorempixel.com/100/100/people/#{pic_uid}",
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    employment_role: role.sample
-    )
-end
 
 # 50.times do
 #   ct = CompanyTool.create!(
