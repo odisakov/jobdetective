f65c612c8495ed5ed28aaae62ae69f432d86cef2
Daniel Levelev <levelev@gmail.com>
2017-12-06 11:52:05 +0100

    algolia vor frontend

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,13 +1,13 @@
 class CompaniesController < ApplicationController
   def index
 
-    # @companies = Company.all if params[:tool].blank?
-    # if params[:tool]
-    #   # @companies = Company.joins(:company_tools).joins(:tools).where(:tools => { name: [params[:tool].split(",")] }).distinct
-    #   @companies = Company.joins(:company_tools).joins(:tools).where("tools.name ILIKE ?", "%#{ params[:tool] }%" ).distinct
-    # else
-    #   @companies = Company.all
-    # end
+    @companies = Company.all if params[:tool].blank?
+    if params[:tool]
+      # @companies = Company.joins(:company_tools).joins(:tools).where(:tools => { name: [params[:tool].split(",")] }).distinct
+      @companies = Company.joins(:company_tools).joins(:tools).where("tools.name ILIKE ?", "%#{ params[:tool] }%" ).distinct
+    else
+      @companies = Company.all
+    end
   end
 
   def show
